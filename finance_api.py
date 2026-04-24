91db023fe276e50d9a1d6cf4cbc62847f3267d03
samarthvasudev045-rgb <samarthvasudev045@gmail.com>
2026-04-24 10:48:48 +0530

    Update finance_api.py

--- finance_api.py
+++ finance_api.py
@@ -1,1 +1,9 @@
+import yfinance as yf
 
+def get_stock_data(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        df = stock.history(period="1mo")
+        return df
+    except:
+        return None
