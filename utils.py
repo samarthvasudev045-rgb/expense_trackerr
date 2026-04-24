9944c0a400bccd0ca06a4f9cf5b2b0dae0bc5d0e
samarthvasudev045-rgb <samarthvasudev045@gmail.com>
2026-04-24 10:48:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,23 @@
+from db import connect
 
+def add_expense(expense):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO expenses (title, amount, category, date) VALUES (?, ?, ?, ?)",
+        expense.to_tuple()
+    )
+
+    conn.commit()
+    conn.close()
+
+def view_expenses():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM expenses")
+    rows = cursor.fetchall()
+
+    conn.close()
+    return rows
